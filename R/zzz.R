0649151eccad330f3784ab44d2aae5e09f952303
Matthew Grainger <matthew.grainger@nina.no>
2020-06-11 13:03:33 +0200

    start vignette

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+#' @export
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to Shiny Application for visualising and predicting lynx harvest. If you have any problems please contact: matthew.grainger@nina.no")
+}
+
